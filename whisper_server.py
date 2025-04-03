14c7b451ccbd9385fd650d8806e6c0117a951b07
Prabhanshu <prabhanshua@gmail.com>
2025-04-04 00:51:09 +0530

    cuda support, turbo model, logging time

--- whisper_server.py
+++ whisper_server.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
 from faster_whisper import WhisperModel
 import tempfile
+import logging
+import time
+
+logging.basicConfig()
+logging.getLogger("faster_whisper").setLevel(logging.DEBUG)
 
 app = FastAPI()
 
 # Load Whisper model (modify for performance)
-model = WhisperModel("tiny", device="cpu", compute_type="float32")
+model = WhisperModel("turbo", device="cuda", compute_type="float16")
 
 @app.post("/transcribe/")
 async def transcribe(audio: UploadFile = File(...)):
-    print("printing audio: ", audio)
+    start_time = time.time()
     with tempfile.NamedTemporaryFile(delete=False) as tmp:
         tmp.write(await audio.read())
         tmp_path = tmp.name
@@ -17,6 +22,8 @@ async def transcribe(audio: UploadFile = File(...)):
     segments, _ = model.transcribe(tmp_path)
 
     transcript = " ".join(segment.text for segment in segments)
+    logging.logger.info(f"Transcription: {transcript}")
+    logging.logger.info(f"Execution Time: {time.time() - start_time:.4f} secconds")
     return {"transcription": transcript}
 
 # Run using: uvicorn whisper_server:app --host 0.0.0.0 --port 8000
