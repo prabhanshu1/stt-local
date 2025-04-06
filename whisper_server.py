422ec69961861f2f4ffda44af6a027bf54057e7a
Prabhanshu <prabhanshua@gmail.com>
2025-04-07 00:35:30 +0530

    using settings from huggingface

--- whisper_server.py
+++ whisper_server.py
@@ -30,7 +30,7 @@
 
 # OpenAPI whisper
 from fastapi import FastAPI, UploadFile, File
-from transformers import pipeline
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 import torch
 import tempfile
 import shutil
@@ -40,8 +40,27 @@ import time
 app = FastAPI()
 
 # Load the Whisper model pipeline
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-pipe = pipeline("automatic-speech-recognition", model="openai/whisper-large-v3-turbo",torch_dtype=torch_dtype, device=0 if torch.cuda.is_available() else -1)
+
+model_id = "openai/whisper-large-v3-turbo"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
 
 @app.post("/transcribe/")
 async def transcribe_audio(audio: UploadFile = File(...)):
